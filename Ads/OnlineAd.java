562edced74b45559777161932ab2bc44f5864a07
Basil Papadimas <basil@excession.gr>
2023-05-23 15:55:07 +0300

    sympathy for the devil

--- Ads/OnlineAd.java
+++ Ads/OnlineAd.java
@@ -2,8 +2,8 @@ package Ads;
 public class OnlineAd extends Ad{
     private int autoShow;
 
-    public OnlineAd(String UID, String typeCode, String productCode, int durationInDays, String details, int autoShow){
-        super(UID, typeCode, productCode, durationInDays, details);
+    public OnlineAd(String typeCode, String productCode, int durationInDays, String details, int autoShow){
+        super(typeCode, productCode, durationInDays, details);
         this.autoShow = autoShow;
     }
     
